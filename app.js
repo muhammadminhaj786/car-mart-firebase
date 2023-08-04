16e6b34aa51d69d2355bf4ba2e23f2847c6a7fb7
Muhammad Minhaj Wahid <122244089+muhammadminhaj786@users.noreply.github.com>
2023-08-04 07:20:05 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,65 +1,38 @@
+import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-auth.js";
+import { getFirestore, collection, addDoc, getDocs, doc, updateDoc, setDoc } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-firestore.js";
 
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
-  import {  getFirestore, collection, addDoc, getDocs, doc, updateDoc, setDoc  } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-firestore.j";
-  import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-auth.js";
+//import auth, db
+import { auth,db } from "./firbaseConfig.js";
 
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+//create a signUp page
+var signupBtn = document.getElementById('signUp')
 
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyAjv2vLOwDpf2OksZEQI3UFP0KBvAfFKZw",
-    authDomain: "car-buy-app.firebaseapp.com",
-    projectId: "car-buy-app",
-    storageBucket: "car-buy-app.appspot.com",
-    messagingSenderId: "1065041727439",
-    appId: "1:1065041727439:web:052da06a0eabdac90e0cba"
-  };
+signupBtn.addEventListener('click',SignUp)
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-
-  // Initialize Cloud Firestore and get a reference to the service
-  const db = getFirestore(app);
-
-  //Initilize auth
-  const auth = getAuth()
+async function SignUp(e){
+  try{
+    var name = document.getElementById('name').value
+  var email = document.getElementById('email').value
+  var password = document.getElementById('password').value
+  // var img = document.getElementById('img-file')
+  var userAuth = await createUserWithEmailAndPassword(auth, email, password)
+  console.log(userAuth.user.uid)
+  const uid = userAuth.user.uid
+  const userObj ={
+    name,
+    email,
+    password,
+    accountActive:true,
+    uid
+  }
+  console.log(userObj)
+  const userRef = doc(db,'users',uid)
+  const userDb = await(setDoc(userRef,userObj))
+  window.location.assign('/')
+  }catch(error){
+    alert(error.message)
+  }
   
-  const signupBtn = document.querySelector("#signinBtn")
-signupBtn.addEventListener("click", signUp)
 
-async function signUp(e) {
-    try {
-        const fullName = document.getElementById("fullName").value
-        const phoneNumber = document.getElementById("phoneNumber").value
-        const email = document.getElementById("email").value
-        const password = document.getElementById("password").value
-
-
-        const userAuth = await createUserWithEmailAndPassword(auth, email, password)
-        console.log(userAuth.user.uid)
-        const uid = userAuth.user.uid
-        const userObj = {
-            fullName,
-            phoneNumber,
-            email,
-            accountActivate: true,
-            uid
-        }
-        console.log("userObj", userObj)
-        const userRef = doc(db, "users", uid);
-        const userDB = await setDoc(userRef, userObj)
-        console.log("userDB", userDB)
-
-        window.location.assign("/")
-
-
-
-
-    } catch (error) {
-        console.log("error", error.message)
-        alert(error.message)
-    }
 
 }
