ec791a43423050636988b2fad60105ac2fddb75e
Muhammad Minhaj Wahid <122244089+muhammadminhaj786@users.noreply.github.com>
2023-08-06 06:59:19 -0700

    half done

--- app.js
+++ app.js
@@ -12,8 +12,19 @@ signupBtn.addEventListener('click',SignUp)
 async function SignUp(e){
   try{
     var name = document.getElementById('name').value
-  var email = document.getElementById('email').value
-  var password = document.getElementById('password').value
+    var email = document.getElementById('email').value
+    var password = document.getElementById('password').value
+    var userType = document.getElementById('userType')
+    //apply conditio user do not fill empty filed
+    if(!name || !email || !password || !userType){
+      alert("required field are missing")
+      return
+    }
+    // if(userType.value===0){
+    //   alert('select user type')
+    //   return
+    // }
+
   // var img = document.getElementById('img-file')
   var userAuth = await createUserWithEmailAndPassword(auth, email, password)
   console.log(userAuth.user.uid)
@@ -23,11 +34,12 @@ async function SignUp(e){
     email,
     password,
     accountActive:true,
-    uid
+    uid,
+    type: userType.value 
   }
   console.log(userObj)
-  const userRef = doc(db,'users',uid)
-  const userDb = await(setDoc(userRef,userObj))
+  // const userRef = doc(db,'users',uid)
+  // const userDb = await(setDoc(userRef,userObj))
   window.location.assign('/')
   }catch(error){
     alert(error.message)
