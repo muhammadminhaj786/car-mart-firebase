93f9586630bddfed94803b3601bca0db4defa6d9
Muhammad Minhaj Wahid <122244089+muhammadminhaj786@users.noreply.github.com>
2023-08-06 07:01:34 -0700

    Update login.js

--- login.js
+++ login.js
@@ -1,5 +1,5 @@
 import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-auth.js";
-import { getFirestore, collection, addDoc, getDocs, doc, updateDoc, setDoc } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-firestore.js";
+import { getFirestore, collection, addDoc, getDocs, getDoc ,doc, updateDoc, setDoc } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-firestore.js";
 
 //import auth, db
 import { auth,db } from "./firbaseConfig.js";
@@ -7,16 +7,38 @@ import { auth,db } from "./firbaseConfig.js";
 //working on login page
 const login = document.getElementById('Login')
 login.addEventListener('click',Login)
-async function Login(){
+async function Login(e){
     try{
-        var email = document.getElementById('email')
-        var password = document.getElementById('password')
+        var email = document.getElementById('email').value
+        var password = document.getElementById('password').value
         const userLogin = await signInWithEmailAndPassword(auth,email,password)
         console.log(userLogin)
-        localStorage.setItem("userUid", userLogin.user.uid)
-        window.location.replace("/dashboard.html")
+        const userRef = doc(db,'users',userLogin.user.uid)
+        const docSnap = await getDoc(userRef);
+
+        if (!docSnap.exists()) {
+            // docSnap.data() will be undefined in this case
+            console.log("No such document!");
+            alert("invalid user")
+            return
+        }
+
+        console.log("Document data:", docSnap.data());
+        const userData = docSnap.data()
+        localStorage.setItem("user", JSON.stringify(userData))
+
+        if (userLogin.type === "admin") {
+            window.location.replace("/adminDashboard.html")
+        } else if (userLogin.type === "Vendor") {
+            window.location.replace("/vendors.html")
+
+        } else if (userLogin.type === "Customer") {
+            window.location.replace("/customer.html")
+
+        }
     }catch(error){
         alert(error.message)
+        console.log(error.message)
     }
 
 }
